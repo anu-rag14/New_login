01c31e88a360d6228826311a84ec5ec550858fc9
Anurag <anuragt0007@gmail.com>
2017-02-17 01:59:54 +0530

    SMS API integrated

--- login/views.py
+++ login/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from .models import login_data
+import requests
+import random
+import urllib # Python URL functions
+import urllib2
 
 # Create your views here.
 @csrf_exempt
@@ -14,7 +18,41 @@ def login_home(request):
 
 		p=login_data.objects.create(name=name,number=number)
 
+		send_otp(name,number)
+
 
 		return render(request,"index.html",{}) 
 
 
+
+
+def send_otp(name,number):
+
+	rand = random.randint(1000,9999)
+
+	authkey = "125195AvX4LUlVf57dcd941"
+	mobiles = "8269936970"
+	message = "Dear " +str(name)+",welcome to CODENICELY this is the one time password "+str(rand)+" for login"
+	sender = "CODNIC"
+	route = "1"
+
+
+	values = {
+    	      'authkey' : authkey,
+        	  'mobiles' : mobiles,
+      	      'message' : message,
+              'sender' : sender,
+              'route' : route
+              }
+
+
+	url="http://api.msg91.com/api/sendhttp.php"
+
+	postdata = urllib.urlencode(values)
+	req = urllib2.Request(url, postdata)
+	response = urllib2.urlopen(req)
+
+	output = response.read() # Get Response
+
+	print output
+	return rand
